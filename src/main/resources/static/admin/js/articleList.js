740d6f920e44da2b452131c465e5a6d59ddee367
vanenea <576700973@qq.com>
2018-11-30 15:34:19 +0800

    30.November.2018

--- src/main/resources/static/admin/js/articleList.js
+++ src/main/resources/static/admin/js/articleList.js
@@ -1,12 +1,30 @@
 var vm = new Vue({
 	el : "#app",
+
 	data : {
-		items : []
+		itemList : [],
+		currentPage : 0,
+		totalPage : 0,
+		count : 0
 	},
 	mounted : function(){
 		this.$nextTick(function(){
 			this.dataMore(1, 9);
+			
 		});
+	},
+	filters:{
+		dataFormat : function(param){
+			
+			var date =  new Date(param*1000);
+			var year = date.getFullYear(),
+				month = date.getMonth()+1,
+				day = date.getDate(),
+				hour = date.getHours(),
+				min = date.getMinutes(),
+				seconds = date.getSeconds();
+			return year+"-"+month+"-"+day+" "+hour+":"+min+":"+seconds;
+		}
 	},
 	methods : {
 		dataMore : function(pageNum, pageSize){
@@ -20,12 +38,32 @@ var vm = new Vue({
 				dataType : "json",
 				success : function(result){
 					if(result && "0000"==result.code){
-						vm.items = result.data;
+						vm.itemList = result.data;
+						vm.currentPage =pageNum,
+						vm.totalPage = result.data.pages,
+						vm.count = pageSize;
+						vm.pageInit();
 					} else {
 						alert(result.msg);
 					}
 				}
 			})
+		},
+		pageInit : function(){
+			$("#pagination").pagination({
+				currentPage: vm.currentPage,
+				totalPage: vm.totalPage,
+				isShow: true,
+				count: vm.count,
+				homePageText: "首页",
+				endPageText: "尾页",
+				prevPageText: "上一页",
+				nextPageText: "下一页",
+				callback: function(current) {
+					$("#current").text(current)
+					vm.dataMore(currentPage, count);
+				}
+			});
 		}
 	}
-});
+})
